2c42688434ec66f8a0ba1ef7540d1605403053b3
Fredrik Haglund <mejl@fredrikhaglund.se>
2016-05-19 14:05:48 +0200

    Skip XML doc for swagger.

--- LMS.Web/App_Start/SwaggerConfig.cs
+++ LMS.Web/App_Start/SwaggerConfig.cs
@@ -96,8 +96,8 @@ namespace LMS.Web
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        c.IncludeXmlComments($@"{System.AppDomain.CurrentDomain.BaseDirectory}\bin\LMS.Web.XML");
-                        c.IncludeXmlComments($@"{System.AppDomain.CurrentDomain.BaseDirectory}\bin\LMS.Data.XML");
+                        //c.IncludeXmlComments($@"{System.AppDomain.CurrentDomain.BaseDirectory}\bin\LMS.Web.XML");
+                        //c.IncludeXmlComments($@"{System.AppDomain.CurrentDomain.BaseDirectory}\bin\LMS.Data.XML");
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
